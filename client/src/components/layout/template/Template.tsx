8793c691dd225caeb1cd8eb15362ad70c30b62fd
crsbl <z.edd36@gmail.com>
2023-06-19 21:12:45 -0400

    back

--- client/src/components/layout/template/Template.tsx
+++ client/src/components/layout/template/Template.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Screen from "../screen";
 import Header from "@/components/ui/header";
 import styles from "./template.module.scss";
-/* import Login from '@/components/functional/Login' */
 
 const Template = ({ children }: { children: any }) => {
   return (
@@ -10,7 +9,6 @@ const Template = ({ children }: { children: any }) => {
       <div className={styles.template00}>
         <Header />
         <div className={styles.template02}>
-          {/* user.email !== "" ? children :  <Login /> */}
           {children}
         </div>
       </div>
